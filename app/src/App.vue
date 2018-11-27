885781229f3706b8b706dd2dd75ee8e2111d5ed5
John Thorson <John.Thorson@hyland.com>
2018-11-27 14:04:54 -0600

    Figuring out vds

---
+++ app/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Test/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+html,
+body,
+#app {
+  min-height: 100%;
+  height: 100%;
+}
+</style>
