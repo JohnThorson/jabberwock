615f1abf68ce8b78c7ef23c04e0672a765bfee46
John Thorson <John.Thorson@hyland.com>
2018-12-13 15:54:18 -0500

    Multi-project site

---
+++ onbase/src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+// Import the Design System
+import DesignSystem from 'vue-design-system'
+import 'vue-design-system/dist/system/system.css'
+
+Vue.use(DesignSystem)
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
